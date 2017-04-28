edfdf285cb3309e45aec11dd612e9d33c6e26099
SANTHOSH KUMAR M <yesemsanthoshkumar@gmail.com>
2017-04-28 08:24:02 +0530

    Added regexes to tagger.py and completion of the function

--- pradyumna/tagger.py
+++ pradyumna/tagger.py
@@ -12,11 +12,17 @@ def tagger(token):
         token       Input word to tag for its part of speech
     """
     pos_regex = [
-                (r'[0-9]+[\.0-9]*', "NUM")      # Numbers
-                (r'\w+', "WD")                  # Any word
+                (r'[0-9]+[.0-9]{,1}', "NUM"),         # Numbers      ==> 32.85, 300
+                (r'am|is|was|are|were', "BVRB"),    # Be verbs,
+                (r'\w+[fl]y', "VB"),                     # Adjectives   ==> beautify
+                (r'\w+ness', "NN"),                  # Noun      ==> effectiveness
+                (r'\w+', "NN"),                      # Any word
     ]
 
-    if re.match(token, pos_regex[0][0]):
-        return pos_regex[0][1]
-    else:
-        return pos_regex[1][1]
+    for regex, tag in pos_regex:
+        if re.match(regex, token.strip()):
+            return tag
+        else:
+            continue
+
+    return "NN"
