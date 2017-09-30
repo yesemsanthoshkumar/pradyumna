eeed4ae018823494143cf752591be5179e5e60ea
yesemsanthoshkumar <yesemsanthoshkumar@gmail.com>
2017-09-30 13:12:08 +0530

    implemented basic workflow

--- pradyumna/pradyumna.py
+++ pradyumna/pradyumna.py
@@ -3,15 +3,19 @@
 __author__ = "yesemsanthoshkumar"
 __version__ = "0.0.1"
 
+import numpy as np
 import pandas as pd
 
 from pymongo import MongoClient
-from vectorizer.tf_idf import tf_idf
-from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
 
 MG_CLIENT = MongoClient("mongodb://localhost//27017")
 
 DB_ = MG_CLIENT["news_docs"]
+MODEL = TfidfVectorizer()
+DF = pd.DataFrame()
+ANSWERING_MODEL = RandomForestClassifier()
 
 def generate_matrix():
     """Generates a dataframe with tfidf vectors from the news documents
@@ -23,27 +27,15 @@ def generate_matrix():
     """
     ids = []
     cont = []
-    for doc in DB_['news'].find().limit(10):
+    for doc in DB_['news'].find().limit(50):
         ids.append(doc['_id'])
+        print(doc["data_section"])
         cont.append(doc['content'])
-    df = pd.DataFrame({'_doc_': ids})
-    cont_df = pd.DataFrame(tf_idf(cont).todense())
-    all_df = pd.concat([df, cont_df], axis=1)
-    return all_df
-
-def train_model(data):
-    """Train a cluster model from the dataframe
-
-    Parameters
-    ----------
-    data    :   pandas.DataFrame
-        The dataframe with the document id and tfidf vectorizer output as features
-    """
-    kmns = KMeans()
-    cols = list(data.columns)
-    cols.remove('_doc_')
-    model = kmns.fit_transform(data[cols])
-    return model
+    trans_content = MODEL.fit_transform(cont)
+    id_frame = pd.DataFrame({'_doc_': ids})
+    cont_frame = pd.DataFrame(trans_content.todense(), columns=MODEL.get_feature_names())
+    df =  pd.concat([id_frame, cont_frame], axis=1).reset_index(drop=True)
+    return df
 
 def awake_bot():
     """Wake up the bot and starts the conversation. My bot loves to sleep :P"""
@@ -57,9 +49,16 @@ def awake_bot():
 
 def generate_response(usr_input):
     """Generates response for the obtained human input"""
-    return "I feel sleepy..."
+    inp = MODEL.transform([usr_input])
+    doc_index = ANSWERING_MODEL.predict(inp.todense())
+    add_to_response = DB_['news'].find_one({
+        '_id': DF.iloc[doc_index]['_doc_'].values[0]
+    })['content']
+    return add_to_response
 
 if __name__ == "__main__":
     DF = generate_matrix()
-    MODEL = train_model(DF)
+    features = list(DF.columns)
+    features.remove('_doc_')
+    ANSWERING_MODEL.fit(DF[features], DF.index)
     awake_bot()
